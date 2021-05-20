d9d37490d751839a499452a4fff9ba52dcc8936f
Zhengwei Gao <zhengwei.gao@yahoo.com>
2021-05-20 14:27:04 +0800

    v0.21.52001 add copyright info; add goroutine to l2trace and biptrace

--- biptrace/worker.go
+++ biptrace/worker.go
@@ -1,7 +1,23 @@
+/*
+Copyright © 2020 Zhengwei Gao<zhengwei.gao@yahoo.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package biptrace
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/zhenggao2/ngapp/utils"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -11,8 +27,8 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
-	"fmt"
 )
 
 const (
@@ -66,123 +82,128 @@ func (p *BipTraceParser) Exec() {
 	}
 
 	if p.pattern == ".pcap"  {
-		var cmd *exec.Cmd
-		var err error
-		var stdOut bytes.Buffer
-		var stdErr bytes.Buffer
-
 		fileInfo, err := ioutil.ReadDir(p.bipTracePath)
 		if err != nil {
 			p.writeLog(zapcore.FatalLevel, fmt.Sprintf("Fail to read directory: %s.", p.bipTracePath))
 			return
 		}
 
+		wg := &sync.WaitGroup{}
 		for _, file := range fileInfo {
 			if !file.IsDir() && path.Ext(file.Name()) == ".pcap" {
-				p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing BIP trace using tshark... [%s]", file.Name()))
-
-				mapEventHeader := make(map[string][]string)
-				mapEventHeaderOk := make(map[string]bool)
-				mapEventRecord := make(map[string]*utils.OrderedMap)
-
-				cmd = exec.Command(path.Join(p.wsharkPath, BIN_TSHARK), "-r", path.Join(p.bipTracePath, file.Name()), "-X", fmt.Sprintf("lua_script:%s", path.Join(p.luasharkPath, LUA_LUASHARK)), "-P", "-V")
-				cmd.Stdout = &stdOut
-				cmd.Stderr = &stdErr
-				p.writeLog(zapcore.DebugLevel, cmd.String())
-				err = cmd.Run()
-				if err != nil {
-					p.writeLog(zapcore.ErrorLevel, err.Error())
-				}
-				if stdOut.Len() > 0 {
-					// TODO use bytes.Buffer.readString("\n") to postprocessing text files
-					p.writeLog(zapcore.InfoLevel, fmt.Sprintf("splitting BIP trace into csv..."))
-					icomRec := false
-					var ts string
-					var event string
-					var fields string
-					for {
-						line, err := stdOut.ReadString('\n')
-						if err != nil {
-							break
-						}
-
-						// remove leading and tailing spaces
-						line = strings.TrimSpace(line)
-
-						if len(line) > 0 {
-							if strings.Contains(line, "ICOM_5G") {
-								icomRec = false
-
-								if len(fields) > 0 {
-									mapEventRecord[event].Add(ts, fields)
-									mapEventHeaderOk[event] = true
-								}
-
-								tokens := strings.Split(line, "/")
-								event = strings.Replace(tokens[len(tokens)-1], ",", "_", -1)
-								if _, exist := mapEventHeader[event]; !exist {
-									mapEventHeader[event] = make([]string, 0)
-									mapEventHeaderOk[event] = false
-								}
-								if _, exist := mapEventRecord[event]; !exist {
-									mapEventRecord[event] = utils.NewOrderedMap()
-								}
-							}
-
-							if strings.Split(line, ":")[0] == "Epoch Time" {
-								// Epoch Time: 1621323617.322338000 seconds
-								tokens := strings.Split(strings.Split(line, " ")[2], ".")
-								sec, _ := strconv.ParseInt(tokens[0], 10, 64)
-								nsec, _ := strconv.ParseInt(tokens[1], 10, 64)
-								ts = time.Unix(sec, nsec).Format("2006-01-02_15:04:05.999999999")
-
-								if !mapEventHeaderOk[event] {
-									mapEventHeader[event] = append(mapEventHeader[event], []string{"eventType", "timestamp"}...)
-								}
-								fields = fmt.Sprintf("%s,%s", event, ts)
-							}
-
-							if line == "ICOM 5G Protocol" {
-								icomRec = true
-							}
-
-							if icomRec {
-								tokens := strings.Split(line, ":")
-								if len(tokens) == 2 {
-									if !mapEventHeaderOk[event] {
-										mapEventHeader[event] = append(mapEventHeader[event], tokens[0])
-									}
-									fields = fields + "," + strings.Replace(strings.TrimSpace(tokens[1]), ",", "_", -1)
-								}
-							}
-						}
-					}
-
-					for k1, v1 := range mapEventHeader {
-						outFn := path.Join(outPath, fmt.Sprintf("%s_%s.csv", path.Base(file.Name()), k1))
-						fout, err := os.OpenFile(outFn, os.O_WRONLY|os.O_CREATE, 0664)
-						if err != nil {
-							p.writeLog(zapcore.ErrorLevel, fmt.Sprintf("Fail to open file: %s", outFn))
-							break
-						}
-
-						fout.WriteString(strings.Join(v1, ",") + "\n")
-						for p := 0; p < mapEventRecord[k1].Len(); p += 1{
-							k2 := mapEventRecord[k1].Keys()[p].(string)
-							v2 := mapEventRecord[k1].Val(k2).(string)
-							fout.WriteString(v2 + "\n")
-						}
-
-						fout.Close()
-					}
-				}
-				if stdErr.Len() > 0 {
-					p.writeLog(zapcore.DebugLevel, stdErr.String())
-				}
-				stdOut.Reset()
-				stdErr.Reset()
+				wg.Add(1)
+				go func(fn string) {
+					defer wg.Done()
+					p.parse(fn)
+				} (file.Name())
 			}
 		}
+		wg.Wait()
+	}
+}
+
+func (p *BipTraceParser) parse(fn string) {
+	p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing BIP trace using tshark... [%s]", fn))
+	outPath := path.Join(p.bipTracePath, "parsed_biptrace")
+
+	mapEventHeader := make(map[string][]string)
+	mapEventHeaderOk := make(map[string]bool)
+	mapEventRecord := make(map[string]*utils.OrderedMap)
+
+	var stdOut bytes.Buffer
+	var stdErr bytes.Buffer
+	cmd := exec.Command(path.Join(p.wsharkPath, BIN_TSHARK), "-r", path.Join(p.bipTracePath, fn), "-X", fmt.Sprintf("lua_script:%s", path.Join(p.luasharkPath, LUA_LUASHARK)), "-P", "-V")
+	cmd.Stdout = &stdOut
+	cmd.Stderr = &stdErr
+	p.writeLog(zapcore.DebugLevel, cmd.String())
+	if err := cmd.Run(); err != nil {
+		p.writeLog(zapcore.ErrorLevel, err.Error())
+	}
+	if stdOut.Len() > 0 {
+		// TODO use bytes.Buffer.readString("\n") to postprocessing text files
+		p.writeLog(zapcore.InfoLevel, fmt.Sprintf("splitting BIP trace into csv... [%s]", fn))
+		icomRec := false
+		var ts string
+		var event string
+		var fields string
+		for {
+			line, err := stdOut.ReadString('\n')
+			if err != nil {
+				break
+			}
+
+			// remove leading and tailing spaces
+			line = strings.TrimSpace(line)
+
+			if len(line) > 0 {
+				if strings.Contains(line, "ICOM_5G") {
+					icomRec = false
+
+					if len(fields) > 0 {
+						mapEventRecord[event].Add(ts, fields)
+						mapEventHeaderOk[event] = true
+					}
+
+					tokens := strings.Split(line, "/")
+					event = strings.Replace(tokens[len(tokens)-1], ",", "_", -1)
+					if _, exist := mapEventHeader[event]; !exist {
+						mapEventHeader[event] = make([]string, 0)
+						mapEventHeaderOk[event] = false
+					}
+					if _, exist := mapEventRecord[event]; !exist {
+						mapEventRecord[event] = utils.NewOrderedMap()
+					}
+				}
+
+				if strings.Split(line, ":")[0] == "Epoch Time" {
+					// Epoch Time: 1621323617.322338000 seconds
+					tokens := strings.Split(strings.Split(line, " ")[2], ".")
+					sec, _ := strconv.ParseInt(tokens[0], 10, 64)
+					nsec, _ := strconv.ParseInt(tokens[1], 10, 64)
+					ts = time.Unix(sec, nsec).Format("2006-01-02_15:04:05.999999999")
+
+					if !mapEventHeaderOk[event] {
+						mapEventHeader[event] = append(mapEventHeader[event], []string{"eventType", "timestamp"}...)
+					}
+					fields = fmt.Sprintf("%s,%s", event, ts)
+				}
+
+				if line == "ICOM 5G Protocol" {
+					icomRec = true
+				}
+
+				if icomRec {
+					tokens := strings.Split(line, ":")
+					if len(tokens) == 2 {
+						if !mapEventHeaderOk[event] {
+							mapEventHeader[event] = append(mapEventHeader[event], tokens[0])
+						}
+						fields = fields + "," + strings.Replace(strings.TrimSpace(tokens[1]), ",", "_", -1)
+					}
+				}
+			}
+		}
+
+		for k1, v1 := range mapEventHeader {
+			outFn := path.Join(outPath, fmt.Sprintf("%s_%s.csv", path.Base(fn), k1))
+			fout, err := os.OpenFile(outFn, os.O_WRONLY|os.O_CREATE, 0664)
+			if err != nil {
+				p.writeLog(zapcore.ErrorLevel, fmt.Sprintf("Fail to open file: %s", outFn))
+				break
+			}
+
+			fout.WriteString(strings.Join(v1, ",") + "\n")
+			for p := 0; p < mapEventRecord[k1].Len(); p += 1{
+				k2 := mapEventRecord[k1].Keys()[p].(string)
+				v2 := mapEventRecord[k1].Val(k2).(string)
+				fout.WriteString(v2 + "\n")
+			}
+
+			fout.Close()
+		}
+	}
+	if stdErr.Len() > 0 {
+		p.writeLog(zapcore.DebugLevel, stdErr.String())
 	}
 }
 
--- l2trace/worker.go
+++ l2trace/worker.go
@@ -1,3 +1,18 @@
+/*
+Copyright © 2020 Zhengwei Gao<zhengwei.gao@yahoo.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package l2trace
 
 import (
@@ -12,6 +27,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -73,150 +89,32 @@ func (p *L2TraceParser) Exec() {
 	}
 
 	if p.pattern == ".pcap"  {
-		var cmd *exec.Cmd
-		var err error
-		var stdOut bytes.Buffer
-		var stdErr bytes.Buffer
-
 		fileInfo, err := ioutil.ReadDir(p.l2TracePath)
 		if err != nil {
 			p.writeLog(zapcore.FatalLevel, fmt.Sprintf("Fail to read directory: %s.", p.l2TracePath))
 			return
 		}
 
+		wg := &sync.WaitGroup{}
 		for _, file := range fileInfo {
 			if !file.IsDir() && path.Ext(file.Name()) == ".pcap" {
-				p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing L2Trace using tshark... [%s]", file.Name()))
-
-				mapEventHeader := make(map[string][]string)
-				mapEventHeaderOk := make(map[string]bool)
-				mapEventRecord := make(map[string]*utils.OrderedMap)
-
-				cmd = exec.Command(path.Join(p.wsharkPath, BIN_TSHARK), "-r", path.Join(p.l2TracePath, file.Name()), "-X", fmt.Sprintf("lua_script:%s", path.Join(p.luasharkPath, LUA_LUASHARK)), "-P", "-V")
-				cmd.Stdout = &stdOut
-				cmd.Stderr = &stdErr
-				p.writeLog(zapcore.DebugLevel, cmd.String())
-				err = cmd.Run()
-				if err != nil {
-					p.writeLog(zapcore.ErrorLevel, err.Error())
-				}
-
-				if stdOut.Len() > 0 {
-					// TODO use bytes.Buffer.readString("\n") to postprocessing text files
-					p.writeLog(zapcore.InfoLevel, fmt.Sprintf("splitting L2Trace into csv..."))
-					icomRec := false
-					pduDump := false
-					payload := false
-					var ts string
-					var event string
-					var fields string
-					for {
-						line, err := stdOut.ReadString('\n')
-						if err != nil {
-							break
-						}
-
-						// remove leading and tailing spaces
-						line = strings.TrimSpace(line)
-
-						if len(line) > 0 {
-							if strings.Contains(line, "ICOM_5G") {
-								icomRec = false
-								pduDump = false
-								payload = false
-
-								if len(fields) > 0 {
-									mapEventRecord[event].Add(ts, fields)
-									mapEventHeaderOk[event] = true
-								}
-							}
-
-							if strings.Split(line, ":")[0] == "Epoch Time" {
-								// Epoch Time: 1621323617.322338000 seconds
-								tokens := strings.Split(strings.Split(line, " ")[2], ".")
-								sec, _ := strconv.ParseInt(tokens[0], 10, 64)
-								nsec, _ := strconv.ParseInt(tokens[1], 10, 64)
-								ts = time.Unix(sec, nsec).Format("2006-01-02_15:04:05.999999999")
-							}
-
-							if line == "ICOM 5G Protocol" {
-								icomRec = true
-							}
-
-							if line == "pduDump-Payload" {
-								pduDump = true
-							}
-
-							if line == "Event-Payload" {
-								payload = true
-
-								line, err := stdOut.ReadString('\n')
-								if err != nil {
-									break
-								}
-								line = strings.TrimSpace(line)
-								event = strings.Split(line, " ")[0]
-								if _, exist := mapEventHeader[event]; !exist {
-									mapEventHeader[event] = make([]string, 0)
-									mapEventHeader[event] = append(mapEventHeader[event], []string{"eventType", "timestamp"}...)
-									mapEventHeaderOk[event] = false
-								}
-								if _, exist := mapEventRecord[event]; !exist {
-									mapEventRecord[event] = utils.NewOrderedMap()
-								}
-								fields = fmt.Sprintf("%s,%s", event, ts)
-							}
-
-							if icomRec && !pduDump && payload {
-								tokens := strings.Split(line, ":")
-								if len(tokens) == 2 {
-									if !mapEventHeaderOk[event] {
-										mapEventHeader[event] = append(mapEventHeader[event], tokens[0])
-									}
-									fields = fields + "," + strings.Replace(strings.TrimSpace(tokens[1]), ",", "_", -1)
-								}
-							}
-						}
-					}
-
-					for k1, v1 := range mapEventHeader {
-						outFn := path.Join(outPath, fmt.Sprintf("%s_%s.csv", path.Base(file.Name()), k1))
-						fout, err := os.OpenFile(outFn, os.O_WRONLY|os.O_CREATE, 0664)
-						if err != nil {
-							p.writeLog(zapcore.ErrorLevel, fmt.Sprintf("Fail to open file: %s", outFn))
-							break
-						}
-
-						fout.WriteString(strings.Join(v1, ",") + "\n")
-						for p := 0; p < mapEventRecord[k1].Len(); p += 1{
-							k2 := mapEventRecord[k1].Keys()[p].(string)
-							v2 := mapEventRecord[k1].Val(k2).(string)
-							fout.WriteString(v2 + "\n")
-						}
-
-						fout.Close()
-					}
-				}
-				if stdErr.Len() > 0 {
-					p.writeLog(zapcore.DebugLevel, stdErr.String())
-				}
-				stdOut.Reset()
-				stdErr.Reset()
+				wg.Add(1)
+				go func(fn string) {
+					defer wg.Done()
+					p.parse(fn)
+				} (file.Name())
 			}
 		}
+		wg.Wait()
 	} else if p.pattern == ".dat" {
 		p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing L2Trace using TLDA..."))
-
-		var cmd *exec.Cmd
-		var err error
 		var stdOut bytes.Buffer
 		var stdErr bytes.Buffer
-		cmd = exec.Command(path.Join(p.py2Path, BIN_PYTHON), path.Join(p.tldaPath, PY_TLDA), "--techlog_path", p.l2TracePath, "--only", "decode", "--components", "UPLANE", "-o", outPath)
+		cmd := exec.Command(path.Join(p.py2Path, BIN_PYTHON), path.Join(p.tldaPath, PY_TLDA), "--techlog_path", p.l2TracePath, "--only", "decode", "--components", "UPLANE", "-o", outPath)
 		cmd.Stdout = &stdOut
 		cmd.Stderr = &stdErr
 		p.writeLog(zapcore.DebugLevel, cmd.String())
-		err = cmd.Run()
-		if err != nil {
+		if err := cmd.Run(); err != nil {
 			p.writeLog(zapcore.ErrorLevel, err.Error())
 		}
 		if stdOut.Len() > 0 {
@@ -225,8 +123,6 @@ func (p *L2TraceParser) Exec() {
 		if stdErr.Len() > 0 {
 			p.writeLog(zapcore.DebugLevel, stdErr.String())
 		}
-		stdOut.Reset()
-		stdErr.Reset()
 
 		decodedDir := path.Join(outPath, "decoded", "UPLANE_snapshot_decoder", "decode")
 		fileInfo, err := ioutil.ReadDir(decodedDir)
@@ -235,145 +131,17 @@ func (p *L2TraceParser) Exec() {
 			return
 		}
 
+		wg := &sync.WaitGroup{}
 		for _, file := range fileInfo {
 			if !file.IsDir() && path.Ext(file.Name()) == ".pcap" {
-				p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing L2Trace using text2pcap/tshark... [%s]", file.Name()))
-
-				mapEventHeader := make(map[string][]string)
-				mapEventHeaderOk := make(map[string]bool)
-				mapEventRecord := make(map[string]*utils.OrderedMap)
-
-				cmd = exec.Command(path.Join(p.wsharkPath, BIN_TEXT2PCAP), "-u", "5678,0", path.Join(decodedDir, file.Name()), path.Join(outPath, file.Name()))
-				cmd.Stdout = &stdOut
-				cmd.Stderr = &stdErr
-				p.writeLog(zapcore.DebugLevel, cmd.String())
-				err = cmd.Run()
-				if err != nil {
-					p.writeLog(zapcore.ErrorLevel, err.Error())
-				}
-				if stdOut.Len() > 0 {
-					// p.writeLog(zapcore.DebugLevel, stdOut.String())
-				}
-				if stdErr.Len() > 0 {
-					// p.writeLog(zapcore.DebugLevel, stdErr.String())
-				}
-				stdOut.Reset()
-				stdErr.Reset()
-
-				cmd = exec.Command(path.Join(p.wsharkPath, BIN_TSHARK), "-r", path.Join(outPath, file.Name()), "-X", fmt.Sprintf("lua_script:%s", path.Join(p.luasharkPath, LUA_LUASHARK)), "-P", "-V")
-				cmd.Stdout = &stdOut
-				cmd.Stderr = &stdErr
-				p.writeLog(zapcore.DebugLevel, cmd.String())
-				err = cmd.Run()
-				if err != nil {
-					p.writeLog(zapcore.ErrorLevel, err.Error())
-				}
-				if stdOut.Len() > 0 {
-					// TODO use bytes.Buffer.readString("\n") to postprocessing text files
-					p.writeLog(zapcore.InfoLevel, fmt.Sprintf("splitting L2Trace into csv..."))
-					icomRec := false
-					pduDump := false
-					payload := false
-					var ts string
-					var event string
-					var fields string
-					for {
-						line, err := stdOut.ReadString('\n')
-						if err != nil {
-							break
-						}
-
-						// remove leading and tailing spaces
-						line = strings.TrimSpace(line)
-
-						if len(line) > 0 {
-							if strings.Contains(line, "ICOM_5G") {
-								icomRec = false
-								pduDump = false
-								payload = false
-
-								if len(fields) > 0 {
-									mapEventRecord[event].Add(ts, fields)
-									mapEventHeaderOk[event] = true
-								}
-							}
-
-							if strings.Split(line, ":")[0] == "Epoch Time" {
-								// Epoch Time: 1621323617.322338000 seconds
-								tokens := strings.Split(strings.Split(line, " ")[2], ".")
-								sec, _ := strconv.ParseInt(tokens[0], 10, 64)
-								nsec, _ := strconv.ParseInt(tokens[1], 10, 64)
-								ts = time.Unix(sec, nsec).Format("2006-01-02_15:04:05.999999999")
-							}
-
-							if line == "ICOM 5G Protocol" {
-								icomRec = true
-							}
-
-							if line == "pduDump-Payload" {
-								pduDump = true
-							}
-
-							if line == "Event-Payload" {
-								payload = true
-
-								line, err := stdOut.ReadString('\n')
-								if err != nil {
-									break
-								}
-								line = strings.TrimSpace(line)
-								event = strings.Split(line, " ")[0]
-								if _, exist := mapEventHeader[event]; !exist {
-									mapEventHeader[event] = make([]string, 0)
-									mapEventHeader[event] = append(mapEventHeader[event], []string{"eventType", "timestamp"}...)
-									mapEventHeaderOk[event] = false
-								}
-								if _, exist := mapEventRecord[event]; !exist {
-									mapEventRecord[event] = utils.NewOrderedMap()
-								}
-								fields = fmt.Sprintf("%s,%s", event, ts)
-							}
-
-							if icomRec && !pduDump && payload {
-								tokens := strings.Split(line, ":")
-								if len(tokens) == 2 {
-									if !mapEventHeaderOk[event] {
-										mapEventHeader[event] = append(mapEventHeader[event], tokens[0])
-									}
-									fields = fields + "," + strings.Replace(strings.TrimSpace(tokens[1]), ",", "_", -1)
-								}
-							}
-						}
-					}
-
-					for k1, v1 := range mapEventHeader {
-						outFn := path.Join(outPath, fmt.Sprintf("%s_%s.csv", strings.Replace(path.Base(file.Name()), "uplane_ttitrace_decoder_", "", -1), k1))
-						fout, err := os.OpenFile(outFn, os.O_WRONLY|os.O_CREATE, 0664)
-						if err != nil {
-							p.writeLog(zapcore.ErrorLevel, fmt.Sprintf("Fail to open file: %s", outFn))
-							break
-						}
-
-						fout.WriteString(strings.Join(v1, ",") + "\n")
-						for p := 0; p < mapEventRecord[k1].Len(); p += 1{
-							k2 := mapEventRecord[k1].Keys()[p].(string)
-							v2 := mapEventRecord[k1].Val(k2).(string)
-							fout.WriteString(v2 + "\n")
-						}
-
-						fout.Close()
-					}
-				}
-				if stdErr.Len() > 0 {
-					p.writeLog(zapcore.DebugLevel, stdErr.String())
-				}
-				stdOut.Reset()
-				stdErr.Reset()
-
-				// remove intermediate pcap
-				os.Remove(path.Join(outPath, file.Name()))
+				wg.Add(1)
+				go func(fn string) {
+					defer wg.Done()
+					p.parse(fn)
+				} (file.Name())
 			}
 		}
+		wg.Wait()
 
 		// remove decoded folder
 		decodedDir = path.Join(outPath, "decoded")
@@ -381,6 +149,158 @@ func (p *L2TraceParser) Exec() {
 			panic(fmt.Sprintf("Fail to remove directory: %v", err))
 		}
 	}
+}
+
+func (p *L2TraceParser) parse(fn string) {
+	if p.pattern == ".pcap" {
+		p.parseWithTshark(fn)
+	} else if p.pattern == ".dat" {
+		p.convert2Pcap(fn)
+		p.parseWithTshark(fn)
+	}
+}
+
+func (p *L2TraceParser) convert2Pcap(fn string) {
+	p.writeLog(zapcore.InfoLevel, fmt.Sprintf("converting L2Trace using text2pcap... [%s]", fn))
+	outPath := path.Join(p.l2TracePath, "parsed_l2trace")
+	decodedDir := path.Join(outPath, "decoded", "UPLANE_snapshot_decoder", "decode")
+
+	var stdOut bytes.Buffer
+	var stdErr bytes.Buffer
+	cmd := exec.Command(path.Join(p.wsharkPath, BIN_TEXT2PCAP), "-u", "5678,0", path.Join(decodedDir, fn), path.Join(outPath, fn))
+	cmd.Stdout = &stdOut
+	cmd.Stderr = &stdErr
+	p.writeLog(zapcore.DebugLevel, cmd.String())
+	if err := cmd.Run(); err != nil {
+		p.writeLog(zapcore.ErrorLevel, err.Error())
+	}
+	if stdOut.Len() > 0 {
+		// p.writeLog(zapcore.DebugLevel, stdOut.String())
+	}
+	if stdErr.Len() > 0 {
+		// p.writeLog(zapcore.DebugLevel, stdErr.String())
+	}
+}
+
+func (p *L2TraceParser) parseWithTshark(fn string) {
+	p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing L2Trace using tshark... [%s]", fn))
+	outPath := path.Join(p.l2TracePath, "parsed_l2trace")
+
+	mapEventHeader := make(map[string][]string)
+	mapEventHeaderOk := make(map[string]bool)
+	mapEventRecord := make(map[string]*utils.OrderedMap)
+
+	var stdOut bytes.Buffer
+	var stdErr bytes.Buffer
+	cmd := exec.Command(path.Join(p.wsharkPath, BIN_TSHARK), "-r", path.Join(outPath, fn), "-X", fmt.Sprintf("lua_script:%s", path.Join(p.luasharkPath, LUA_LUASHARK)), "-P", "-V")
+	cmd.Stdout = &stdOut
+	cmd.Stderr = &stdErr
+	p.writeLog(zapcore.DebugLevel, cmd.String())
+	if err := cmd.Run(); err != nil {
+		p.writeLog(zapcore.ErrorLevel, err.Error())
+	}
+	if stdOut.Len() > 0 {
+		// TODO use bytes.Buffer.readString("\n") to postprocessing text files
+		p.writeLog(zapcore.InfoLevel, fmt.Sprintf("splitting L2Trace into csv... [%s]", fn))
+		icomRec := false
+		pduDump := false
+		payload := false
+		var ts string
+		var event string
+		var fields string
+		for {
+			line, err := stdOut.ReadString('\n')
+			if err != nil {
+				break
+			}
+
+			// remove leading and tailing spaces
+			line = strings.TrimSpace(line)
+
+			if len(line) > 0 {
+				if strings.Contains(line, "ICOM_5G") {
+					icomRec = false
+					pduDump = false
+					payload = false
+
+					if len(fields) > 0 {
+						mapEventRecord[event].Add(ts, fields)
+						mapEventHeaderOk[event] = true
+					}
+				}
+
+				if strings.Split(line, ":")[0] == "Epoch Time" {
+					// Epoch Time: 1621323617.322338000 seconds
+					tokens := strings.Split(strings.Split(line, " ")[2], ".")
+					sec, _ := strconv.ParseInt(tokens[0], 10, 64)
+					nsec, _ := strconv.ParseInt(tokens[1], 10, 64)
+					ts = time.Unix(sec, nsec).Format("2006-01-02_15:04:05.999999999")
+				}
+
+				if line == "ICOM 5G Protocol" {
+					icomRec = true
+				}
+
+				if line == "pduDump-Payload" {
+					pduDump = true
+				}
+
+				if line == "Event-Payload" {
+					payload = true
+
+					line, err := stdOut.ReadString('\n')
+					if err != nil {
+						break
+					}
+					line = strings.TrimSpace(line)
+					event = strings.Split(line, " ")[0]
+					if _, exist := mapEventHeader[event]; !exist {
+						mapEventHeader[event] = make([]string, 0)
+						mapEventHeader[event] = append(mapEventHeader[event], []string{"eventType", "timestamp"}...)
+						mapEventHeaderOk[event] = false
+					}
+					if _, exist := mapEventRecord[event]; !exist {
+						mapEventRecord[event] = utils.NewOrderedMap()
+					}
+					fields = fmt.Sprintf("%s,%s", event, ts)
+				}
+
+				if icomRec && !pduDump && payload {
+					tokens := strings.Split(line, ":")
+					if len(tokens) == 2 {
+						if !mapEventHeaderOk[event] {
+							mapEventHeader[event] = append(mapEventHeader[event], tokens[0])
+						}
+						fields = fields + "," + strings.Replace(strings.TrimSpace(tokens[1]), ",", "_", -1)
+					}
+				}
+			}
+		}
+
+		for k1, v1 := range mapEventHeader {
+			outFn := path.Join(outPath, fmt.Sprintf("%s_%s.csv", strings.Replace(path.Base(fn), "uplane_ttitrace_decoder_", "", -1), k1))
+			fout, err := os.OpenFile(outFn, os.O_WRONLY|os.O_CREATE, 0664)
+			if err != nil {
+				p.writeLog(zapcore.ErrorLevel, fmt.Sprintf("Fail to open file: %s", outFn))
+				break
+			}
+
+			fout.WriteString(strings.Join(v1, ",") + "\n")
+			for p := 0; p < mapEventRecord[k1].Len(); p += 1{
+				k2 := mapEventRecord[k1].Keys()[p].(string)
+				v2 := mapEventRecord[k1].Val(k2).(string)
+				fout.WriteString(v2 + "\n")
+			}
+
+			fout.Close()
+		}
+	}
+	if stdErr.Len() > 0 {
+		p.writeLog(zapcore.DebugLevel, stdErr.String())
+	}
+
+	// remove intermediate pcap
+	os.Remove(path.Join(outPath, fn))
 }
 
 func (p *L2TraceParser) writeLog(level zapcore.Level, s string) {
