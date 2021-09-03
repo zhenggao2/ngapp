220b80a4dabb2f9ecd57458abf99e3cdde0a94e4
Zhengwei Gao <zhengwei.gao@yahoo.com>
2021-09-04 01:14:36 +0800

    v0.21.090303: DDR4 parser can now output FD of each frame0-slot0-symbol0 to csv

--- ddr4trace/worker.go
+++ ddr4trace/worker.go
@@ -16,14 +16,23 @@ limitations under the License.
 package ddr4trace
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	cmap "github.com/orcaman/concurrent-map"
 	"github.com/zhenggao2/ngapp/utils"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gonum.org/v1/gonum/dsp/fourier"
 	"io/ioutil"
+	"math/cmplx"
 	"os"
+	"os/exec"
 	"path"
+	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -32,20 +41,26 @@ const (
 	BIN_PYTHON3         string = "python.exe"
 	BIN_PYTHON3_LINUX   string = "python3"
 	PY3_SNAPSHOT_TOOL string = "SnapshotAnalyzer.py"
+	LOKI_IQ_NORM_FACTOR int = 16384
 )
 
 type Ddr4TraceParser struct {
 	log          *zap.Logger
+	py3Path string
+	snapToolPath string
 	ddr4TracePath string
 	pattern      string
 	maxgo int
 	debug        bool
 
 	traceFiles []string
+	iqdata cmap.ConcurrentMap
 }
 
-func (p *Ddr4TraceParser) Init(log *zap.Logger, trace, pattern string, maxgo int, debug bool) {
+func (p *Ddr4TraceParser) Init(log *zap.Logger, py3, snaptool, trace, pattern string, maxgo int, debug bool) {
 	p.log = log
+	p.py3Path = py3
+	p.snapToolPath = snaptool
 	p.ddr4TracePath = trace
 	p.pattern = pattern
 	p.maxgo = utils.MaxInt([]int{2, maxgo})
@@ -64,6 +79,8 @@ func (p *Ddr4TraceParser) Init(log *zap.Logger, trace, pattern string, maxgo int
 			p.traceFiles = append(p.traceFiles, path.Join(p.ddr4TracePath, file.Name()))
 		}
 	}
+
+	p.iqdata = cmap.New()
 }
 
 func (p *Ddr4TraceParser) Exec() {
@@ -104,11 +121,133 @@ func (p *Ddr4TraceParser) Exec() {
 		}
 		wg.Wait()
 	}
+
+	for _, key := range p.iqdata.Keys() {
+		m, _ := p.iqdata.Get(key)
+		p.writeLog(zapcore.InfoLevel, fmt.Sprintf("iqdata: key=%v, len=%v", key, len(m.([]complex128))))
+		/*
+		for i := 0; i < 100; i++ {
+			p.writeLog(zapcore.DebugLevel, fmt.Sprintf("iq = %v", m.([]complex128)[i]))
+		}
+		 */
+
+		rf0sl0symb0u := m.([]complex128)[320:4416]
+		fft := fourier.NewCmplxFFT(len(rf0sl0symb0u))
+		coeff := fft.Coefficients(nil, rf0sl0symb0u)
+
+		iq := make([]complex128, 0)
+		amp := make([]float64, 0)
+		for i := range coeff {
+			i := fft.ShiftIdx(i)
+			iq = append(iq, coeff[i])
+			amp = append(amp, cmplx.Abs(coeff[i]))
+		}
+
+		p.writeLog(zapcore.DebugLevel, fmt.Sprintf("Frame 0 Slot 0 Symbol 0: len=%v", len(amp)))
+		fout, err := os.OpenFile(path.Join(outPath, strings.Split(key, ".")[0] + "_frame0_slot0_symbol0.csv"), os.O_WRONLY|os.O_CREATE, 0664)
+		if err != nil {
+			p.writeLog(zapcore.ErrorLevel, err.Error())
+			return
+		}
+
+		for i := range iq {
+			fout.WriteString(fmt.Sprintf("%v,%v,%v,%v\n", i+1, real(iq[i]), imag(iq[i]), amp[i]))
+		}
+
+		fout.Close()
+	}
 }
 
 func (p *Ddr4TraceParser) parse(fn string) {
+	// 1st step: parse DDR4(.bin) using Loki snapshot_tool
 	p.writeLog(zapcore.InfoLevel, fmt.Sprintf("parsing DDR4 trace using Loki snapshot_tool... [%s]", fn))
-	// outPath := path.Join(p.ddr4TracePath, "parsed_biptrace")
+	outPath := path.Join(p.ddr4TracePath, "parsed_ddr4trace")
+	decodedDir := path.Join(outPath, strings.Split(fn, ".")[0] + "_result")
+
+	var stdOut bytes.Buffer
+	var stdErr bytes.Buffer
+	var cmd *exec.Cmd
+	if runtime.GOOS == "linux" {
+		cmd = exec.Command(path.Join(p.py3Path, BIN_PYTHON3_LINUX), path.Join(p.snapToolPath, "SnapshotAnalyzer.py"), "--iqdata", path.Join(p.ddr4TracePath, fn), "--output", decodedDir)
+	} else if runtime.GOOS == "windows" {
+		cmd = exec.Command(path.Join(p.py3Path, BIN_PYTHON3), path.Join(p.snapToolPath, "SnapshotAnalyzer.py"), "--iqdata", path.Join(p.ddr4TracePath, fn), "--output", decodedDir)
+	} else {
+		p.writeLog(zapcore.ErrorLevel, fmt.Sprintf("Unsupported OS: runtime.GOOS=%s", runtime.GOOS))
+		return
+	}
+	cmd.Stdout = &stdOut
+	cmd.Stderr = &stdErr
+	p.writeLog(zapcore.DebugLevel, cmd.String())
+	if err := cmd.Run(); err != nil {
+		p.writeLog(zapcore.ErrorLevel, err.Error())
+	}
+	if stdOut.Len() > 0 {
+		p.writeLog(zapcore.DebugLevel, stdOut.String())
+	}
+	if stdErr.Len() > 0 {
+		p.writeLog(zapcore.DebugLevel, stdErr.String())
+	}
+
+	// 2nd step: parsing ant_x.txt where x=0..nap-1
+	antFiles := make([]string, 0)
+	subcells, _ := filepath.Glob(path.Join(path.Join(decodedDir, "nr", "ul"), "subcellid*"))
+	for _, sc := range subcells {
+		ants, _ := filepath.Glob(path.Join(sc, "ant*.txt"))
+		antFiles = append(antFiles, ants...)
+	}
+
+	if p.debug {
+		p.writeLog(zapcore.DebugLevel, fmt.Sprintf("fn = %v, antFiles=%v\n", fn, antFiles))
+	}
+
+	wg := &sync.WaitGroup{}
+	for _, file := range antFiles {
+		for {
+			if runtime.NumGoroutine() >= p.maxgo {
+				time.Sleep(1 * time.Second)
+			} else {
+				break
+			}
+		}
+
+		wg.Add(1)
+		go func(fn string) {
+			defer wg.Done()
+
+			p.writeLog(zapcore.InfoLevel, fmt.Sprintf("Loading per antenna I/Q samples... [%s]", fn))
+			key := strings.Replace(strings.Replace(fn, path.Join(p.ddr4TracePath, "parsed_ddr4trace"), "ddr4", -1), "/", "_", -1)
+			p.iqdata.SetIfAbsent(key, make([]complex128, 0))
+
+			fin, err := os.Open(fn)
+			if err != nil {
+				p.writeLog(zapcore.ErrorLevel, err.Error())
+				return
+			}
+
+			reader := bufio.NewReader(fin)
+			for {
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					break
+				}
+
+				// remove leading and tailing spaces
+				line = strings.TrimSpace(line)
+				if len(line) == 0 {
+					continue
+				}
+
+				iq := strings.Split(line, " ")
+				i, _ := strconv.ParseFloat(iq[0], 64)
+				q, _ := strconv.ParseFloat(iq[1], 64)
+
+				m, _ := p.iqdata.Get(key)
+				m = append(m.([]complex128), complex(i/float64(LOKI_IQ_NORM_FACTOR), q/float64(LOKI_IQ_NORM_FACTOR)))
+				p.iqdata.Set(key, m)
+			}
+		} (file)
+	}
+	wg.Wait()
 }
 
 func (p *Ddr4TraceParser) writeLog(level zapcore.Level, s string) {
--- go.mod
+++ go.mod
@@ -3,16 +3,22 @@ module github.com/zhenggao2/ngapp
 go 1.14
 
 require (
-	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
-	github.com/beevik/etree v1.1.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible
+	github.com/ajstarks/svgo v0.0.0-20210406150507-75cfd577ce75 // indirect
+	github.com/beevik/etree v1.1.0
+	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20190411002643-bd77b112433e // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/orcaman/concurrent-map v0.0.0-20210501183033-44dafcb38ecc // indirect
-	github.com/spf13/afero v1.1.2 // indirect
+	github.com/orcaman/concurrent-map v0.0.0-20210501183033-44dafcb38ecc
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.7.1
-	github.com/therecipe/qt v0.0.0-20200904063919-c0c124a5770d
-	github.com/unidoc/unioffice v1.12.0 // indirect
+	github.com/unidoc/unioffice v1.12.0
 	go.uber.org/zap v1.16.0
+	golang.org/x/exp v0.0.0-20210903013509-41231fe85c93 // indirect
+	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	gonum.org/v1/gonum v0.9.3 // indirect
+	gonum.org/v1/plot v0.9.0
 )
